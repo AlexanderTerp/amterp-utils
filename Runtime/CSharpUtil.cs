8b96152db5acec9facf6c673dae4ce9aa48018f3
Alexander Terp <alexander.terp@gmail.com>
2022-04-25 09:43:08 +1000

    Organize methods and add TimeSpan.FormatAsHHMMSS
    Let's try to structure the utils class a bit but providing headings and
    moving things around. Also add a new extension method to TimeSpan which
    lets us print e.g. a 1m5s timespan as 01:05, with some room for
    customization e.g. 1:05 instead.

--- Runtime/CSharpUtil.cs
+++ Runtime/CSharpUtil.cs
@@ -8,10 +8,7 @@ namespace Amterp.Utils {
         private static readonly DateTime EPOCH = new DateTime(1970, 1, 1, 0, 0, 0);
         private static System.Random RNG = new System.Random();
 
-        public static void ForEach<T>(this IEnumerable<T> enumerable, Action<T> action) {
-            foreach (var item in enumerable)
-                action(item);
-        }
+        // STANDALONE STATICS
 
         public static long CurrentTimeMillis() {
             return DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
@@ -21,9 +18,39 @@ namespace Amterp.Utils {
             return (long)((DateTime.UtcNow - EPOCH).TotalMilliseconds * 1000000.0);
         }
 
-        public static T Last<T>(this T[] array) {
-            return array[array.Length - 1];
+        // FORMATTING
+
+        public static string FormatAsPercent(this float thisFloat) {
+            return string.Format("{0:P1}", thisFloat);
         }
+
+        // todo clean up with a StringBuilder, if C# has one.
+        public static string FormatAsHHMMSS(this TimeSpan timeSpan,
+                bool includeHoursIfZero = false,
+                bool includeMinutesIfZero = false,
+                bool includeLeadingZeroInHours = false,
+                bool includeLeadingZeroInMinutes = false,
+                bool includeLeadingZeroInSeconds = false) {
+            int hours = timeSpan.Hours;
+            int minutes = timeSpan.Minutes;
+            int seconds = timeSpan.Seconds;
+            if (hours > 0 || includeHoursIfZero) {
+                if (includeLeadingZeroInHours) return string.Format("{0:D2}:{1:D2}:{2:D2}", hours, minutes, seconds);
+                return string.Format("{0:D1}:{1:D2}:{2:D2}", hours, minutes, seconds);
+            }
+            if (minutes > 0 || includeMinutesIfZero) {
+                if (includeLeadingZeroInMinutes) return string.Format("{0:D2}:{1:D2}", minutes, seconds);
+                return string.Format("{0:D1}:{1:D2}", minutes, seconds);
+            }
+            if (includeLeadingZeroInSeconds) return string.Format("{0:D2}", seconds);
+            return string.Format("{0:D1}", seconds);
+        }
+
+        public static string ToDetailedString(this Vector3 vector3) {
+            return $"({vector3.x}, {vector3.y}, {vector3.z})";
+        }
+
+        // UNITY EXTENSIONS
 
         public static Vector2 ToVector2DroppingY(this Vector3 vector3) {
             return new Vector2(vector3.x, vector3.z);
@@ -37,16 +64,24 @@ namespace Amterp.Utils {
             return new Vector3(v.x, 0, v.y);
         }
 
+
+        // C# EXTENSIONS
+
+        public static void ForEach<T>(this IEnumerable<T> enumerable, Action<T> action) {
+            foreach (var item in enumerable)
+                action(item);
+        }
+
+        public static T Last<T>(this T[] array) {
+            return array[array.Length - 1];
+        }
+
         public static T Sample<T>(this IList<T> collection) {
             return collection[RNG.Next(0, collection.Count - 1)];
         }
 
         public static string Capitalize(this string thisString) {
             return char.ToUpper(thisString[0]) + thisString.Substring(1);
-        }
-
-        public static string ToDetailedString(this Vector3 vector3) {
-            return $"({vector3.x}, {vector3.y}, {vector3.z})";
         }
 
         public static float normalized(this float thisFloat) {
@@ -57,10 +92,6 @@ namespace Amterp.Utils {
 
         public static bool isAboutZero(this float thisFloat) {
             return Mathf.Approximately(0, thisFloat);
-        }
-
-        public static string FormatAsPercent(this float thisFloat) {
-            return string.Format("{0:P1}", thisFloat);
         }
 
         public static void SafeInvoke<T>(this Action<T> action, T arg) {
